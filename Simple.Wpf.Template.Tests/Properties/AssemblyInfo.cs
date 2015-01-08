b5bef448ba9246ae460d3732277c7827352c3139
Ollie Riches <ollie.riches@btinternet.com>
2015-01-08 18:26:08 +0000

    Updated assembly info in test project

--- Simple.Wpf.Template.Tests/Properties/AssemblyInfo.cs
+++ Simple.Wpf.Template.Tests/Properties/AssemblyInfo.cs
@@ -4,13 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-using Simple.Wpf.Template.Tests;
 
-[assembly: AssemblyTitle("WpfTemplate.Tests")]
+[assembly: AssemblyTitle("Simple.Wpf.Template.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfTemplate.Tests")]
+[assembly: AssemblyProduct("Simple.Wpf.Template.Tests")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
