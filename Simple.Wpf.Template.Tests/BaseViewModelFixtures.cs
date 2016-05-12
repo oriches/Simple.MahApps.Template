d9c65e4ed4544c0343f022da92e646580807a2c0
Ollie Riches <ollie.riches@btinternet.com>
2016-05-12 19:11:17 +0100

    Fxied base test fixture class

--- Simple.Wpf.Template.Tests/BaseViewModelFixtures.cs
+++ Simple.Wpf.Template.Tests/BaseViewModelFixtures.cs
@@ -15,7 +15,7 @@ namespace Simple.Wpf.Template.Tests
 
         public Mock<IGestureService> GestureService { get; private set; }
 
-        [OneTimeSetUp]
+        [SetUp]
         public void BaseSetup()
         {
             GestureService = new Mock<IGestureService>();
@@ -24,6 +24,7 @@ namespace Simple.Wpf.Template.Tests
             ObservableExtensions.GestureService = GestureService.Object;
 
             TestScheduler = new TestScheduler();
+            TestScheduler.AdvanceTo(DateTimeOffset.Now.Ticks);
 
             SchedulerService = new MockSchedulerService(TestScheduler);
         }
